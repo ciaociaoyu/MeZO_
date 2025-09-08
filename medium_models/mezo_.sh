85997a92078144013bed30b253cf45ccbcc0bf17
ciaociaoyu <111696707+ciaociaoyu@users.noreply.github.com>
2025-09-07 20:43:02 -0400

    Create mezo_.sh

---
+++ medium_models/mezo_.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+TASK=${TASK:-SST-2}
+K=${K:-16}
+SEED=${SEED:-42}
+BS=${BS:-64}
+LR=${LR:-1e-6}
+EPS=${EPS:-1e-3}
+WD=${WD:-0}
+# This is true seting
+STEP=${STEP:-100}
+EVAL_STEP=${EVAL_STEP:-10}
+# This is for testing
+# STEP=${STEP:-100}
+# EVAL_STEP=${EVAL_STEP:-10}
+MODEL=${MODEL:-roberta-large}
+USE_H=${USE_H:-"True"}
+USE_C=${USE_C:-"False"}
+USE_LH=${USE_LH:-"True"}
+
+LOGITS=$(jq -n '{"SNLI": 3, "MNLI": 3, "trec": 6, "sst-5": 5}["'$TASK'"] // 2')
+
+echo "TASK: $TASK"
+echo "K: $K"
+echo "Seed: $SEED"
+echo "BS: $BS"
+echo "LR: $LR"
+echo "EPS: $EPS"
+echo "Step: $STEP; Eval step: $EVAL_STEP"
+echo "Using adaptive h: $USE_H"
+
+GR_TAG=seed$SEED-bs$BS-lr$LR-eps$EPS-wd$WD-step$STEP-evalstep$EVAL_STEP
+EXTRA_TAG=${EXTRA_TAG:-ft}
+TAG=${TAG:-k${K}-${MODEL}-mezo-${EXTRA_TAG}}
+echo "Grid search tag: $GR_TAG"
+echo "Tag: $TAG"
+
+TYPE=prompt GRID_TAG=$GR_TAG TAG=$TAG STEPS=$STEP TASK=$TASK SEED=$SEED MODEL=$MODEL K=$K \
+    bash run_fewshot.sh --per_device_train_batch_size $BS --learning_rate $LR --eval_steps $EVAL_STEP --weight_decay $WD --zero_order_eps $EPS --use_adaptive_h $USE_H --use_c_scale $USE_C --use_layerwise_h $USE_LH\
+    --zero_order_optim --lr_scheduler_type "constant" --optimizer "sgd" --efficient_zero_order \
+    $@
